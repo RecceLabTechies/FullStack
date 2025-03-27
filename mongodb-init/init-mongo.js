2f8e19c0e4abac8833fa9a788e5a90380680819e
Javian Ng <j4vianz01@gmail.com>
2025-03-27 23:00:48 +0800

    update initialisation of mongo to include campaign_performance

--- mongodb-init/init-mongo.js
+++ mongodb-init/init-mongo.js
@@ -26,3 +26,50 @@ if (!usersCollectionExists) {
 } else {
   print("Users collection already exists. Skipping initialization.");
 }
+
+// Check if the campaign_performance collection exists
+const campaignCollectionExists = collections.includes("campaign_performance");
+
+// If the campaign_performance collection doesn't exist, create it and set up schema
+if (!campaignCollectionExists) {
+  print("Campaign performance collection does not exist. Creating it...");
+
+  // Create a validator for the collection to enforce schema
+  db.createCollection("campaign_performance", {
+    validator: {
+      $jsonSchema: {
+        bsonType: "object",
+        required: [
+          "date",
+          "campaign_id",
+          "channel",
+          "age_group",
+          "ad_spend",
+          "views",
+          "leads",
+          "new_accounts",
+          "country",
+          "revenue",
+        ],
+        properties: {
+          date: { bsonType: "date" },
+          campaign_id: { bsonType: "string" },
+          channel: { bsonType: "string" },
+          age_group: { bsonType: "string" },
+          ad_spend: { bsonType: "double" },
+          views: { bsonType: "double" },
+          leads: { bsonType: "double" },
+          new_accounts: { bsonType: "double" },
+          country: { bsonType: "string" },
+          revenue: { bsonType: "double" },
+        },
+      },
+    },
+  });
+
+  print("Campaign performance collection created with schema validation.");
+} else {
+  print(
+    "Campaign performance collection already exists. Skipping initialization."
+  );
+}
